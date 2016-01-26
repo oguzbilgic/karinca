abcc786a1207d1d1fd5fae3f7c54dff84ab83f52
Oguz Bilgic <fisyonet@gmail.com>
2016-01-26 14:17:28 -0800

    Extract {common, ui}.clj from core.clj

---
+++ src/karinca/common.clj
@@ -0,0 +1,13 @@
+(ns karinca.common
+  (:use karinca.constants))
+
+;world is a 2d vector of refs to cells
+(def world
+     (apply vector
+            (map (fn [_]
+                   (apply vector (map (fn [_] (ref (struct cell 0 0)))
+                                      (range dim))))
+                 (range dim))))
+
+(defn place [[x y]]
+  (-> world (nth x) (nth y)))
--- src/karinca/core.clj
+++ src/karinca/core.clj
@@ -1,16 +1,7 @@
 (ns karinca.core
-  (:use karinca.constants))
-
-;world is a 2d vector of refs to cells
-(def world
-     (apply vector
-            (map (fn [_]
-                   (apply vector (map (fn [_] (ref (struct cell 0 0)))
-                                      (range dim))))
-                 (range dim))))
-
-(defn place [[x y]]
-  (-> world (nth x) (nth y)))
+  (:use karinca.constants
+        karinca.common
+        karinca.ui))
 
 (defn create-ant
   "create an ant at the location, returning an ant agent on the location"
@@ -183,86 +174,6 @@
       (let [p (place [x y])]
         (alter p assoc :pher (* evap-rate (:pher @p))))))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; UI ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(import
- '(java.awt Color Graphics Dimension)
- '(java.awt.image BufferedImage)
- '(javax.swing JPanel JFrame))
-
-;pixels per world cell
-(def scale 5)
-
-(defn fill-cell [#^Graphics g x y c]
-  (doto g
-    (.setColor c)
-    (.fillRect (* x scale) (* y scale) scale scale)))
-
-(defn render-ant [ant #^Graphics g x y]
-  (let [black (. (new Color 0 0 0 255) (getRGB))
-        gray (. (new Color 100 100 100 255) (getRGB))
-        red (. (new Color 255 0 0 255) (getRGB))
-        [hx hy tx ty] ({0 [2 0 2 4]
-                        1 [4 0 0 4]
-                        2 [4 2 0 2]
-                        3 [4 4 0 0]
-                        4 [2 4 2 0]
-                        5 [0 4 4 0]
-                        6 [0 2 4 2]
-                        7 [0 0 4 4]}
-                       (:dir ant))]
-    (doto g
-      (.setColor (if (:food ant)
-                  (new Color 255 0 0 255)
-                  (new Color 0 0 0 255)))
-      (.drawLine (+ hx (* x scale)) (+ hy (* y scale))
-                (+ tx (* x scale)) (+ ty (* y scale))))))
-
-(defn render-place [g p x y]
-  (when (pos? (:pher p))
-    (fill-cell g x y (new Color 0 255 0
-                          (int (min 255 (* 255 (/ (:pher p) pher-scale)))))))
-  (when (pos? (:food p))
-    (fill-cell g x y (new Color 255 0 0
-                          (int (min 255 (* 255 (/ (:food p) food-scale)))))))
-  (when (:ant p)
-    (render-ant (:ant p) g x y)))
-
-(defn render [g]
-  (let [v (dosync (apply vector (for [x (range dim) y (range dim)]
-                                   @(place [x y]))))
-        img (new BufferedImage (* scale dim) (* scale dim)
-                 (. BufferedImage TYPE_INT_ARGB))
-        bg (. img (getGraphics))]
-    (doto bg
-      (.setColor (. Color white))
-      (.fillRect 0 0 (. img (getWidth)) (. img (getHeight))))
-    (dorun
-     (for [x (range dim) y (range dim)]
-       (render-place bg (v (+ (* x dim) y)) x y)))
-    (doto bg
-      (.setColor (. Color blue))
-      (.drawRect (* scale home-off) (* scale home-off)
-                 (* scale nants-sqrt) (* scale nants-sqrt)))
-    (. g (drawImage img 0 0 nil))
-    (. bg (dispose))))
-
-(def panel (doto (proxy [JPanel] []
-                        (paint [g] (render g)))
-             (.setPreferredSize (new Dimension
-                                     (* scale dim)
-                                     (* scale dim)))))
-
-(def frame (doto (new JFrame) (.add panel) .pack .show))
-
-(def animator (agent nil))
-
-(defn animation [x]
-  (when running
-    (send-off *agent* #'animation))
-  (. panel (repaint))
-  (. Thread (sleep animation-sleep-ms))
-  nil)
-
 (def evaporator (agent nil))
 
 (defn evaporation [x]
@@ -271,7 +182,6 @@
   (evaporate)
   (. Thread (sleep evap-sleep-ms))
   nil)
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; use ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn -main
